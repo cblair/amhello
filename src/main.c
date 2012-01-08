66139a19fbb85c08ecf77e4e3448423954361994
Colby Blair <cblair@inspiron.(none)>
2012-01-08 08:26:17 -0800

    CPP and C build together

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include <config.h>
           #include <stdio.h>
           
+extern int main_cpp(void);
+
           int
           main (void)
           {
+		main_cpp();
             puts ("Hello World!");
             puts ("This is " PACKAGE_STRING ".");
             return 0;
